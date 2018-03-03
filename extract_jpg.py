f8f43db697e7c5de36e9f681187723e7d292f20d
xericho <xeric.hox@gmail.com>
2018-03-03 11:29:34 -0800

    Update extract_jpg.py

--- extract_jpg.py
+++ extract_jpg.py
@@ -1,11 +1,14 @@
 import numpy as np
 from PIL import Image
 
-f = np.load('females.npy')
+data=np.load('face_dataset20k_part0.npy')
+labels=np.load('all_labels.npy')
+f = data[labels==-1]
 
 for i in range(3):
 	print('Extracting image {}...'.format(i))
 	im = Image.fromarray(f[i])
+	### CHANGE PATH
 	im.save('./data/female{:06d}.jpg'.format(i))
 
 
