d69bb39ce9c5fb007a7aa6c05463184dc7fa3c08
xericho <xeric.hox@gmail.com>
2018-03-03 11:29:52 -0800

    Update extract_jpg.py

--- extract_jpg.py
+++ extract_jpg.py
@@ -5,7 +5,7 @@ data=np.load('face_dataset20k_part0.npy')
 labels=np.load('all_labels.npy')
 f = data[labels==-1]
 
-for i in range(3):
+for i in range(f.shape[0]):
 	print('Extracting image {}...'.format(i))
 	im = Image.fromarray(f[i])
 	### CHANGE PATH
